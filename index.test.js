3679b6d4dfed8e8d86656ea8e142435b7a54c62a
Stephanie Jordan <sjordan@middlebury.edu>
2019-09-12 15:35:45 -0400

    bday practical

--- index.test.js
+++ index.test.js
@@ -28,26 +28,16 @@ describe('Determines age based on birthday', () => {
   test('Returns 0 if birthday is today', () => {
     expect(birthday.howOld(new Date('01 Jan 2018'))).toBe(0);
   });
-});
 
-describe('Determines age based on birthday', () => {
-  let _Date;
-  beforeAll(() => {
-    // Save original date module
-    _Date = Date;
-  });
-
-  afterAll(() => {
-    // Reset Date
-    Date = _Date;
-  });
-
-  beforeEach(() => {
-    // Set a fixed date
-    Date.now = jest.fn(() => new Date('01 Jan 2015').valueOf());
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('01 Jan 2017'))).toBe(1);
   });
 
   test('Returns 0 if birthday is today', () => {
-    expect(birthday.howOld(new Date('01 Jan 2015'))).toBe(0);
+    expect(birthday.howOld(new Date('31 Dec 2018'))).toBe(0);
+  });
+
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('30 Jan 2015'))).toBe(2);
   });
 });
